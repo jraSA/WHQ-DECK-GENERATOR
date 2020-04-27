cd68ad44326c4aea1fa644f5eaf4af20879eca30
Jorge Rodriguez <jorge.rodriguez@piksel.com>
2020-04-27 08:41:59 +0200

    DungeonTable refactor

--- src/organisms/DungeonTable/DungeonTable.tsx
+++ src/organisms/DungeonTable/DungeonTable.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { View } from 'react-native';
 import styled from 'styled-components/native';
 
 import dungeonDeck, { back, objectiveInstances } from '../../assets/dungeonDeck';
+import Card from '../../atoms/Card';
 import shuffle from '../../helpers/shuffle/Shuffle';
 import Deck from '../../molecules/Deck';
 import Table from '../../molecules/Table';
@@ -13,11 +15,31 @@ const DeckContainer = styled.View`
 `;
 
 const DungeonTable = (props: { navigation: any }) => {
+    const getRandomInstance = () => Math.floor(Math.random() * objectiveInstances.length);
+
+    const [objectiveInstanceIndex, setObjectiveInstanceIndex] = useState(getRandomInstance());
+
+    const resetDungeonDeck = () => {
+        setObjectiveInstanceIndex(getRandomInstance());
+    };
+
+    const getObjectiveInstanceIndexInsideDeck = (draft: any[]) => draft.length - 6 + Math.floor(Math.random() * 6);
+
+    const generateDungeonDeck = () => {
+        const draft = shuffle(dungeonDeck).slice(0, 11);
+        const index = getObjectiveInstanceIndexInsideDeck(draft);
+
+        draft.splice(index, 0, objectiveInstances[objectiveInstanceIndex]);
+        return draft;
+    };
+
     const renderItems = () => {
         return (
             <DeckContainer>
-                <Deck generateDeck={() => shuffle(objectiveInstances)} back={back} />
-                <Deck generateDeck={() => shuffle(dungeonDeck)} back={back} />
+                <View>
+                    <Card source={objectiveInstances[objectiveInstanceIndex]} onRevert={resetDungeonDeck} />
+                </View>
+                <Deck generateDeck={generateDungeonDeck} back={back} />
             </DeckContainer>
         );
     };
