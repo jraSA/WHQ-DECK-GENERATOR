99eabc20c8045aaac69ccfec60872cbb49dcd14d
Jorge Rodriguez <jorge.rodriguez@piksel.com>
2020-05-03 14:01:11 +0200

    Refactor Dungeon table

--- src/organisms/DungeonTable/DungeonTable.tsx
+++ src/organisms/DungeonTable/DungeonTable.tsx
@@ -1,49 +1,11 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
-import styled from 'styled-components/native';
+import React from 'react';
 
-import dungeonDeck, { back, objectiveInstances } from '../../assets/dungeonDeck';
-import Card from '../../atoms/Card';
-import shuffle from '../../helpers/shuffle/Shuffle';
-import Deck from '../../molecules/Deck';
+import DungeonDeck from '../../molecules/DungeonDeck';
 import Table from '../../molecules/Table';
 
-const DeckContainer = styled.View`
-    flex: 1;
-    flex-direction: row;
-    align-self: center;
-`;
-
 const DungeonTable = (props: { navigation: any }) => {
-    const getRandomInstance = () => Math.floor(Math.random() * objectiveInstances.length);
-
-    const [objectiveInstanceIndex, setObjectiveInstanceIndex] = useState(getRandomInstance());
-
-    const getObjectiveInstanceIndexInsideDeck = (draft: any[]) => draft.length - 6 + Math.floor(Math.random() * 6);
-
-    const generateDungeonDeck = () => {
-        const draft = shuffle(dungeonDeck).slice(0, 11);
-        const index = getObjectiveInstanceIndexInsideDeck(draft);
-
-        draft.splice(index, 0, objectiveInstances[objectiveInstanceIndex]);
-        return draft;
-    };
-    const [initialDeck, setInitialDeck] = useState(generateDungeonDeck());
-
-    const resetDungeonDeck = () => {
-        setObjectiveInstanceIndex(getRandomInstance());
-        setInitialDeck(generateDungeonDeck());
-    };
-
     const renderItems = () => {
-        return (
-            <DeckContainer>
-                <View>
-                    <Card source={objectiveInstances[objectiveInstanceIndex]} onRevert={resetDungeonDeck} />
-                </View>
-                <Deck generateDeck={generateDungeonDeck} back={back} initialDeck={initialDeck} />
-            </DeckContainer>
-        );
+        return <DungeonDeck />;
     };
 
     return <Table renderItems={renderItems} navigation={props.navigation} />;
